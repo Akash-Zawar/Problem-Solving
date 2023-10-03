87f012a9d62978681bada7770227ba677ffc94c8
Akash-Zawar <zawarakash1707@gmail.com>
2023-10-03 20:53:37 +0530

    update in  frequency sort

--- 24.frequescySort.js
+++ 24.frequescySort.js
@@ -1,10 +1,3 @@
-/**
- *
- * find occurence of each character in string
- * then add in it a string based on there occurence in sorted order
- *
- */
-
 const freqencySort = (a) => {
   let p = {};
   let ans = "";
@@ -15,10 +8,21 @@ const freqencySort = (a) => {
       p[a[i]] = 1;
     }
   }
-  for (let key in p) {
-    ans += key.repeat(p[key]);
+  //   console.log(p);
+  let values = Object.values(p);
+  values.sort((a, b) => b - a);
+  //   console.log(values);
+
+  for (let i = 0; i < values.length; i++) {
+    for (let key in p) {
+      if (values[i] === p[key]) {
+        ans += key.repeat(p[key]);
+        delete p[key];
+      }
+    }
   }
+
   return ans;
 };
 
-console.log(freqencySort("aaaaaabbbBBBAAcC"));
+console.log(freqencySort("abcAbcAASDDSDbbsC"));
