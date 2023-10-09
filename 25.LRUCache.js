5079c898794752670373c0f647437241dc9ad5be
Akash-Zawar <zawarakash1707@gmail.com>
2023-10-09 20:28:12 +0530

    updated LRUCache

--- 25.LRUCache.js
+++ 25.LRUCache.js
@@ -1,26 +1,37 @@
 let myList = new Map();
 
-const LRUCache = (k) => {
+const LRUCache = (k, val) => {
+  let myListSize = val;
   let temp = 0;
-
-  if (myList.get(k) == undefined) {
-    myList.set(k, 1);
+  if (myList.size < myListSize) {
+    if (myList.get(k) == undefined) {
+      myList.set(k, 1);
+    } else {
+      temp = myList.get(k);
+      myList.delete(k);
+      myList.set(k, temp + 1);
+    }
   } else {
-    temp = myList.get(k);
-    myList.delete(k);
-    myList.set(k, temp + 1);
+    myList.delete(myList.keys().next().value);
+    if (myList.get(k) == undefined) {
+      myList.set(k, 1);
+    } else {
+      temp = myList.get(k);
+      myList.delete(k);
+      myList.set(k, temp + 1);
+    }
   }
 };
 
-LRUCache(1);
-LRUCache(2);
-LRUCache(4);
-LRUCache(2);
-LRUCache(5);
-LRUCache(6);
-LRUCache(7);
-LRUCache(1);
-LRUCache(2);
-LRUCache(7);
+LRUCache(1, 3);
+LRUCache(2, 3);
+LRUCache(4, 3);
+LRUCache(2, 3);
+LRUCache(5, 3);
+LRUCache(6, 3);
+LRUCache(7, 3);
+LRUCache(1, 3);
+LRUCache(2, 3);
+LRUCache(7, 3);
 
 console.log(myList);
